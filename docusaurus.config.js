920161ae404715c589169edf4578345190d089b3
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2022-03-08 17:16:09 +0530

    chore(): update edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/Synthesized-Infinity/Ethereal-Docs/tree/master/docs/',
+          editUrl: 'https://github.com/Synthesized-Infinity/Ethereal-Docs/tree/master/',
         },
         blog: {
           showReadingTime: true,
