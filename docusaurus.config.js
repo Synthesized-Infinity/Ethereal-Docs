64c6e5ee6d1161e0d9beae31c1dc0f0fbf5a221c
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2022-03-08 17:08:40 +0530

    chore(): update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Synthesized Infinity.`,
       },
       prism: {
         theme: lightCodeTheme,
